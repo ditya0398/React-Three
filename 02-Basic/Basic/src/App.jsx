07a7e074a74d7a9515f4990f55283f98874f96c2
ditya0398 <aditya.maheshwari033@gmail.com>
2023-07-23 14:04:35 +0530

    Cleaned the code

--- 02-Basic/Basic/src/App.jsx
+++ 02-Basic/Basic/src/App.jsx
@@ -4,38 +4,8 @@ import {useThree, useFrame} from '@react-three/fiber'
 import { useRef } from 'react'
 // import the canvas element from r3f
 import { Canvas } from "@react-three/fiber";
+import Cube from "./Three-Code/Cube";
 
-function Cube()
-{
- 
-    const scene = useThree((state) => state.scene)
-    const renderer = useThree((state) => state.gl)
-    const aspect = window.innerWidth / window.innerHeight
-    const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100)
-    camera.position.set(1, 1, 2)
-    camera.lookAt(0,0,0)
-    scene.add(camera)
-    
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshStandardMaterial({ color: 0x781CE5 })
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
-    
-    const ambient = new THREE.AmbientLight(0x404040, 5)
-    const point = new THREE.PointLight(0xE4FF00, 1, 10)
-    point.position.set(3, 3, 2)
-    scene.add(ambient)
-    scene.add(point)
-    console.log("Hello");
-    const ref = useRef();
-     useFrame((state, delta, xrFrame) => {
-       console.log("I am inside the useFrame");
-       renderer.render(scene, camera)
-      // console.log("Finished rendering");
-     });
-
-
-}
 export default function App() {
   return (
     <div
--- 02-Basic/Basic/src/Three-Code/Cube.jsx
+++ 02-Basic/Basic/src/Three-Code/Cube.jsx
@@ -4,29 +4,29 @@ import { useRef } from 'react'
 export default function Cube()
 {
  
-    // const scene = useThree((state) => state.scene)
- 
-    // const aspect = window.innerWidth / window.innerHeight
-    // const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100)
-    // camera.position.set(1, 1, 2)
-    // camera.lookAt(0,0,0)
-    // scene.add(camera)
-    
-    // const geometry = new THREE.BoxGeometry(1, 1, 1)
-    // const material = new THREE.MeshStandardMaterial({ color: 0x781CE5 })
-    // const mesh = new THREE.Mesh(geometry, material)
-    // scene.add(mesh)
-    
-    // const ambient = new THREE.AmbientLight(0x404040, 5)
-    // const point = new THREE.PointLight(0xE4FF00, 1, 10)
-    // point.position.set(3, 3, 2)
-    // scene.add(ambient)
-    // scene.add(point)
-    console.log("Hello");
-    const ref = useRef();
-    useFrame((state, delta, xrFrame) => {
-      console.log("I am inside the useFrame");
-    });
-
-    return (<Cube/>);
+  const scene = useThree((state) => state.scene)
+  const renderer = useThree((state) => state.gl)
+  const aspect = window.innerWidth / window.innerHeight
+  const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100)
+  camera.position.set(1, 1, 2)
+  camera.lookAt(0,0,0)
+  scene.add(camera)
+  
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshStandardMaterial({ color: 0x781CE5 })
+  const mesh = new THREE.Mesh(geometry, material)
+  scene.add(mesh)
+  
+  const ambient = new THREE.AmbientLight(0x404040, 5)
+  const point = new THREE.PointLight(0xE4FF00, 1, 10)
+  point.position.set(3, 3, 2)
+  scene.add(ambient)
+  scene.add(point)
+  console.log("Hello");
+  const ref = useRef();
+   useFrame((state, delta, xrFrame) => {
+     console.log("I am inside the useFrame of CubeJSX");
+     renderer.render(scene, camera)
+    // console.log("Finished rendering");
+   });
 }
