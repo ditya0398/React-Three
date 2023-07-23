df3b68e08a0cba50c377405d6485a112b5ddd2a0
ditya0398 <aditya.maheshwari033@gmail.com>
2023-07-23 13:06:40 +0530

    *Added basic skeleton app

---
+++ 02-Basic/Basic/src/App.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+// import the canvas element from r3f
+import { Canvas } from "@react-three/fiber";
+
+export default function App() {
+  return (
+    // create the canvas
+    <Canvas>
+      // create an ambient light
+      <ambientLight />
+      
+      // create a pointlight and give it a position
+      <pointLight position={[10, 10, 10]} />
+    
+      // create a mess and rotate it slightly
+      <mesh rotation={[10, 15, 6]}>
+          
+        // create a box geometry of a size of 2 on all axis 
+        <boxGeometry args={[2, 2, 2]} />
+            
+        // give the mesh the standard pbr material with preset color of hotpink
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+    </Canvas>
+  );
+}
